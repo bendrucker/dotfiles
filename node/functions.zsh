ce1f3a819dff914fd85afb06814b2f31f567c39a
Ben Drucker <bvdrucker@gmail.com>
2016-09-01 23:35:55 -0700

    Add `publish` and `module` helper fns for npm

---
+++ node/functions.zsh
@@ -0,0 +1,17 @@
+publish () {
+  git fetch
+  if [[ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]]; then
+    return 1
+  fi
+  npm version $1 && git push origin --tags && npm publish
+}
+
+module () {
+  cd $PROJECTS
+  mkdir $1
+  cd $1
+  git init
+  hub create
+  travis enable --no-interactive &
+  yo bd
+}
