6fc175e2cdb6b7af747f51ddf06cb4ca9ab52302
Ben Drucker <bvdrucker@gmail.com>
2016-09-02 08:15:12 -0700

    Inline rbenv init script to append to PATH instead of prepend

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,4 +1,21 @@
 if (( $+commands[rbenv] ))
 then
-  eval "$(rbenv init -)"
+  export PATH="${PATH}:~/.rbenv/shims"
+  export RBENV_SHELL=zsh
+  source '/usr/local/Cellar/rbenv/1.0.0/libexec/../completions/rbenv.zsh'
+  command rbenv rehash 2>/dev/null
+  rbenv() {
+    local command
+    command="$1"
+    if [ "$#" -gt 0 ]; then
+      shift
+    fi
+
+    case "$command" in
+    rehash|shell)
+      eval "$(rbenv "sh-$command" "$@")";;
+    *)
+      command rbenv "$command" "$@";;
+    esac
+  }
 fi
