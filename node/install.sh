c6b984d14fc5ae6c1971ba18ec96f515ead54db6
Ben Drucker <bvdrucker@gmail.com>
2016-09-08 09:50:07 -0700

    Add more global node packages to node/install script

--- node/install.sh
+++ node/install.sh
@@ -1,2 +1,9 @@
-# https://github.com/princejwesley/Mancy
+# fancy repl
 npm install -g mancy
+
+# markdown preview
+npm install -g vmd
+
+# debug via electron/chrome dev tools
+npm install -g devtool
+s
