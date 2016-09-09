96ee1db65117b0e5fdcc573194df034d45d1b256
Ben Drucker <bvdrucker@gmail.com>
2016-09-08 23:15:39 -0700

    Add droplr fn

---
+++ system/droplr.zsh
@@ -0,0 +1,12 @@
+droplr() {
+  if [[ $# -eq 0 ]]; then
+      echo You need to specify a parameter. >&2
+      return 1
+  fi
+
+  if [[ "$1" =~ ^http[|s]:// ]]; then
+      osascript -e "tell app 'Droplr' to shorten '$1'"
+  else
+      open -ga /Applications/Droplr.app "$1"
+  fi
+}
