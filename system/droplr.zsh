9583d706412da41c9f7e03bcff929e3285796f45
Ben Drucker <bvdrucker@gmail.com>
2016-09-08 23:20:17 -0700

    Add Droplr script

--- system/droplr.zsh
+++ system/droplr.zsh
@@ -1,12 +1,15 @@
+# urls: droplr http://web.site
+# files: droplr file.txt
+
 droplr() {
   if [[ $# -eq 0 ]]; then
-      echo You need to specify a parameter. >&2
-      return 1
+    echo 'droplr <url|file>' >&2
+    return 1
   fi
 
-  if [[ "$1" =~ ^http[|s]:// ]]; then
-      osascript -e "tell app 'Droplr' to shorten '$1'"
+  if [[ "$1" =~ ^http[s]?:// ]]; then
+    osascript -e "tell app 'Droplr' to shorten '$1'"
   else
-      open -ga /Applications/Droplr.app "$1"
+    open -ga /Applications/Droplr.app "$1"
   fi
 }
