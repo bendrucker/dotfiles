5d59d1ef073d45ba9ecbf82217b977ec27d8c4dc
Ben Drucker <bvdrucker@gmail.com>
2016-09-12 14:49:24 -0700

    Add pre-configured eaze csr script

---
+++ eaze/csr.zsh
@@ -0,0 +1,10 @@
+eaze-csr () {
+  local country='NL'
+  local state='CA'
+  local city='San Francisco'
+  local organization='Eaze Solutions, Inc.'
+  local unit='Engineering'
+  local fqdn=$1
+
+  csr $fqdn -subj "/C=$country/ST=$state/L=$city/O=$organization/OU=$unit/CN=$fqdn"
+}
