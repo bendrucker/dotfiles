b16712617bb9e9d5eab157a335101e2d96d677ea
Ben Drucker <bvdrucker@gmail.com>
2016-09-14 12:03:37 -0700

    Add restart script for Eaze supervisor services

--- eaze/chef.zsh
+++ eaze/chef.zsh
@@ -3,3 +3,9 @@ eaze-converge () {
   echo "converging $ENVIRONMENT"
   knife ssh "chef_environment:$ENVIRONMENT" "sudo chef-client"
 }
+
+eaze-restart () {
+  local ENVIRONMENT="$1-$2"
+  echo "restarting $ENVIRONMENT"
+  knife ssh "chef_environment:$ENVIRONMENT" "sudo supervisorctl restart all"
+}
