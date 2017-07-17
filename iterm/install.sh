54ea4a266c5e137aafac8e5dac0e7f355f2d5b2a
Ben Drucker <bvdrucker@gmail.com>
2017-07-17 12:07:09 -0700

    add iterm prefs

---
+++ iterm/install.sh
@@ -0,0 +1,4 @@
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$ZSH/iterm"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
