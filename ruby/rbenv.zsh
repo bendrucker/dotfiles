f0e2d0bffdfacbe3bd3563ecdbd242ff610e1c8a
Ben Drucker <bvdrucker@gmail.com>
2017-07-17 12:13:59 -0700

    revert to holman's rbenv init script

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,21 +1,4 @@
 if (( $+commands[rbenv] ))
 then
-  export PATH="$HOME/.rbenv/shims:$PATH"
-  export RBENV_SHELL=zsh
-  source '/usr/local/Cellar/rbenv/1.0.0/libexec/../completions/rbenv.zsh'
-  command rbenv rehash 2>/dev/null
-  rbenv() {
-    local command
-    command="$1"
-    if [ "$#" -gt 0 ]; then
-      shift
-    fi
-
-    case "$command" in
-    rehash|shell)
-      eval "$(rbenv "sh-$command" "$@")";;
-    *)
-      command rbenv "$command" "$@";;
-    esac
-  }
+  eval "$(rbenv init -)"
 fi
