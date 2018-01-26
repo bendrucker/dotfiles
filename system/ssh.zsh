d8b8b2ee19a124b3dfb4c5390bc4cd30504ee211
Ben Drucker <bvdrucker@gmail.com>
2018-01-25 21:30:43 -0800

    add id_rsa ssh key if one isn't present

--- system/ssh.zsh
+++ system/ssh.zsh
@@ -1,1 +1,10 @@
 alias keygen="ssh-keygen -t rsa -b 4096 -N '' -f"
+
+local default="$HOME/.ssh/id_rsa"
+
+if [ ! -f "$default" ]
+then
+  mkdir -p "$(dirname $default)"
+  keygen "$default" -C "$(git config --get user.email)"
+  ssh-add "$default"
+fi
