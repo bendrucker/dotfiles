2d3572434b67d9646c5597c6fdab0a4d6df00136
Ben Drucker <bvdrucker@gmail.com>
2018-01-25 17:54:04 -0800

    Add sublime configuration

---
+++ sublime/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -euf
+
+PACKAGE_CONTROL_URL="https://packagecontrol.io/Package%20Control.sublime-package"
+DATA="$HOME/Library/Application Support/Sublime Text 3"
+PACKAGES="$DATA/Installed Packages"
+PACKAGE_CONTROL_PATH="$PACKAGES/Package Control.sublime-package"
+SETTINGS="$DATA/Packages/User"
+
+
+mkdir -p "$DATA"
+mkdir -p "$PACKAGES"
+
+[ -f "$PACKAGE_CONTROL_PATH" ] || curl --silent "$PACKAGE_CONTROL" > "$PACKAGE_CONTROL_PATH"
+cp -f "$ZSH/sublime/packages.json" "$SETTINGS/Package Control.sublime-settings"
