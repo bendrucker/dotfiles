9f9db72f6aa74a38c8c8f712b574ace3d48a1143
Ben Drucker <bvdrucker@gmail.com>
2018-02-06 15:00:58 -0800

    add dsh alias for running docker -it --entrypoint sh

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -3,4 +3,6 @@
 alias d='docker'
 alias dc='docker-compose'
 
+alias dsh="docker run --entrypoint sh -it"
+
 alias docker-gc="docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /etc:/etc:ro spotify/docker-gc"
