95df7ff3b121865e82655f296f3b95824f470efa
Ben Drucker <bvdrucker@gmail.com>
2019-10-03 17:48:36 -0700

    node: chown /usr/local targets, install latest with n

--- node/install.sh
+++ node/install.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+sudo mkdir -p /usr/local/n
+sudo chown -R "$(whoami)" /usr/local/n
+sudo chown -R "$(whoami)" /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
+
+n latest
+
 [ -f "$HOME/.npmrc" ] || (echo 'npm login:' && npm login)
 npm install --global $(cat "${0:a:h}/globals.txt" | tr '\n' ' ')
 
