ad89f350064be9f165493930e4c2784b53abe9cc
Ben Drucker <bvdrucker@gmail.com>
2020-12-07 13:12:53 -0800

    brew: replace ruby installer with bash

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -13,7 +13,7 @@ then
   # Install the correct homebrew for each OS type
   if test "$(uname)" = "Darwin"
   then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   fi
 
 fi
