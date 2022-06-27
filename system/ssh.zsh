c5044579eaa8e2aa9a892831df090666bb459e68
Ben Drucker <bvdrucker@gmail.com>
2022-06-26 17:33:29 -0700

    mosh: add script for allowing through firewall

--- system/ssh.zsh
+++ system/ssh.zsh
@@ -10,3 +10,16 @@ then
   keygen "$DEFAULT_SSH_KEY" -C "$(git config --get user.email)"
   ssh-add "$DEFAULT_SSH_KEY"
 fi
+
+firewall_allow_mosh_server() {
+  local fw='/usr/libexec/ApplicationFirewall/socketfilterfw'
+  local mosh_sym="$(which mosh-server)"
+  local mosh_abs="$(greadlink -f $mosh_sym)"
+
+  sudo "$fw" --setglobalstate off
+  sudo "$fw" --add "$mosh_sym"
+  sudo "$fw" --unblockapp "$mosh_sym"
+  sudo "$fw" --add "$mosh_abs"
+  sudo "$fw" --unblockapp "$mosh_abs"
+  sudo "$fw" --setglobalstate on
+}
