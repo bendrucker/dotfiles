62da49e3ecfce2a6a0b609a042b9c806c89bbe5e
Ben Drucker <bvdrucker@gmail.com>
2022-07-03 15:21:42 -0700

    go: use `install`

--- go/install.sh
+++ go/install.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-packages=()
 while read -r package; do
-  packages+=("$package")
+  go install "$package@latest"
 done < "$(dirname "$0")/packages"
-
-echo "installing go packages:"
-printf '%s\n' "${packages[@]}"
-
-go get -u "${packages[@]}"
