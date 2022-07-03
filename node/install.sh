a3e10d0d95d372601001b67baf5bbdbf02991854
Ben Drucker <bvdrucker@gmail.com>
2022-07-03 15:23:53 -0700

    node: remove chown for non-existent dirs

--- node/install.sh
+++ node/install.sh
@@ -4,7 +4,6 @@ set -e
 
 sudo mkdir -p /usr/local/n
 sudo chown -R "$(whoami)" /usr/local/n
-sudo chown -R "$(whoami)" /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
 
 n latest
 
