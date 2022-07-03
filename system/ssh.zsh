aa8ae138f7c84b6b317ab5136ffe78a68cccc130
Ben Drucker <bvdrucker@gmail.com>
2022-07-03 14:37:22 -0700

    ssh: fix shell lint (#6)

--- system/ssh.zsh
+++ system/ssh.zsh
@@ -12,9 +12,10 @@ then
 fi
 
 firewall_allow_mosh_server() {
-  local fw='/usr/libexec/ApplicationFirewall/socketfilterfw'
-  local mosh_sym="$(which mosh-server)"
-  local mosh_abs="$(greadlink -f $mosh_sym)"
+  local fw mosh_sym mosh_abs
+  fw='/usr/libexec/ApplicationFirewall/socketfilterfw'
+  mosh_sym="$(which mosh-server)"
+  mosh_abs="$(greadlink -f "$mosh_sym")"
 
   sudo "$fw" --setglobalstate off
   sudo "$fw" --add "$mosh_sym"
