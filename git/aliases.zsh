18aa86757187945ac83a9b95f60acb4c535042b2
Ben Drucker <bvdrucker@gmail.com>
2022-07-25 13:38:32 -0700

    git: rm hub

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,14 +1,5 @@
 #!/usr/bin/env zsh
 
-# Use `hub` as our git wrapper:
-# http://defunkt.github.com/hub/
-
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
-fi
-
 alias g='git'
 
 alias gb='git branch'
