0d86da8efa8473e3b91ab6c52e9eecdcc8d0b1f9
Ben Drucker <bvdrucker@gmail.com>
2022-07-26 11:43:28 -0700

    dot: fix brew inclusion

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -14,7 +14,6 @@ then
   if test "$(uname)" = "Darwin"
   then
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    eval "$(/opt/homebrew/bin/brew shellenv)"
   fi
 
 fi
