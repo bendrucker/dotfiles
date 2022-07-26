9c9ecab23c778873eb1d7da9cfe751c47d9320be
Ben Drucker <bvdrucker@gmail.com>
2022-07-26 11:33:55 -0700

    ssh: RSA -> Ed25519

--- ssh/keygen.zsh
+++ ssh/keygen.zsh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-alias keygen="ssh-keygen -t rsa -b 4096 -N '' -f"
-
-DEFAULT_SSH_KEY="$HOME/.ssh/id_rsa"
+DEFAULT_SSH_KEY="$HOME/.ssh/id_ed25519"
 
 if [ ! -f "$DEFAULT_SSH_KEY" ]
 then
   mkdir -p "$(dirname "$DEFAULT_SSH_KEY")"
-  keygen "$DEFAULT_SSH_KEY" -C "$(git config --get user.email)"
+  ssh-keygen -t ed25519 -f "$DEFAULT_SSH_KEY" -C "$(git config --get user.email)"
   ssh-add "$DEFAULT_SSH_KEY"
 fi
