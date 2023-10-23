b412569795e1439d5245abd958a2a5eb251dd2d2
Ben Drucker <bvdrucker@gmail.com>
2023-10-23 14:59:44 -0700

    docker: remove gc

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -4,5 +4,3 @@ alias d='docker'
 alias dc='docker-compose'
 
 alias dsh="docker run --entrypoint sh -it"
-
-alias docker-gc="docker run --rm --userns host -v /var/run/docker.sock:/var/run/docker.sock -v /etc:/etc spotify/docker-gc"
