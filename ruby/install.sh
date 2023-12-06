2c954ac31d9da9df7289c5f18e9fdf88f47743b5
Ben Drucker <ben.drucker@observeinc.com>
2023-12-06 12:04:46 -0800

    ruby: rm rbenv for asdf

--- ruby/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-version="$(rbenv install -l | grep -v - | tail -1 | tr -d " ")"
-rbenv install "$version"
-rbenv global "$version"
--- ruby/rbenv.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-if (( $+commands[rbenv] ))
-then
-  eval "$(rbenv init -)"
-fi
