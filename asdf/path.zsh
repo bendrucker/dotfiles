4cd326f6742991ac928e24256459afeab08e262e
Ben Drucker <bvdrucker@gmail.com>
2024-02-01 18:10:31 -0800

    asdf: run pathfile as zsh

--- asdf/path.zsh
+++ asdf/path.zsh
@@ -1,10 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
 
 asdf="${HOMEBREW_PREFIX}/opt/asdf/libexec/asdf.sh"
 
 if [ -f "$asdf" ]; then
-  # shellcheck disable=SC1091
-  . "${asdf}"
+  source "${asdf}"
 else
   echo "asdf not found, tools will not be available" >&2
 fi
