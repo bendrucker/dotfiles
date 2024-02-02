1545b45b4ac721a3d96178cfc140e5c286d437dc
Ben Drucker <bvdrucker@gmail.com>
2024-02-01 17:45:39 -0800

    debug asdf path

--- asdf/path.zsh
+++ asdf/path.zsh
@@ -3,4 +3,6 @@
 if [ -f /opt/homebrew/opt/asdf/libexec/asdf.sh ]; then
   # shellcheck disable=SC1091
   . /opt/homebrew/opt/asdf/libexec/asdf.sh
+else
+  echo "asdf not found, tools will not be available" >&2
 fi
