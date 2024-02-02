3441b7d7ed9e3644a162dc487198c567aa06413a
Ben Drucker <bvdrucker@gmail.com>
2024-02-01 18:06:10 -0800

    use $HOMEBREW_PREFIX to reference ASDF

--- asdf/path.zsh
+++ asdf/path.zsh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 
-if [ -f /opt/homebrew/opt/asdf/libexec/asdf.sh ]; then
+asdf="${HOMEBREW_PREFIX}/opt/asdf/libexec/asdf.sh"
+
+if [ -f "$asdf" ]; then
   # shellcheck disable=SC1091
-  . /opt/homebrew/opt/asdf/libexec/asdf.sh
+  . "${asdf}"
 else
   echo "asdf not found, tools will not be available" >&2
 fi
