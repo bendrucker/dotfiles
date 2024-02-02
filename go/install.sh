0273c7f2b85d3217727c79b54822beb8969dd102
Ben Drucker <bvdrucker@gmail.com>
2024-02-01 17:02:10 -0800

    remove old asdf installs

--- go/install.sh
+++ go/install.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-if ! asdf plugin list --urls | grep github.com/asdf-vm/asdf-golang ; then
-  asdf plugin add golang https://github.com/asdf-vm/asdf-golang.git
-else
-  asdf plugin update golang
-fi
-
-asdf install golang
-
 while read -r package; do
   go install "$package@latest"
 done < "$(dirname "$0")/packages"
--- nodejs/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-if ! asdf plugin list --urls | grep github.com/asdf-vm/asdf-nodejs ; then
-    asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-else
-    asdf plugin update nodejs
-fi
