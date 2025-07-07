f6dd671728417e5dac4f2646371b04ae812f0379
Ben Drucker <bvdrucker@gmail.com>
2025-07-06 17:51:14 -0700

    terminal: tmux, merge dirs

--- terminal/install.sh
+++ terminal/install.sh
@@ -5,6 +5,6 @@ if [ "$(uname -s)" != "Darwin" ]; then
 fi
 
 # Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$ZSH/iterm"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$ZSH/terminal"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
