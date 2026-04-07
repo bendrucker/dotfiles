1c13b8e821d80db6df0e1c7480183853e1a65722
Ben Drucker <bvdrucker@gmail.com>
2026-04-07 14:10:44 -0700

    neovim: migrate to Neovim 0.12 built-in features (#358)
    * neovim: migrate to Neovim 0.12 built-in features
    
    Replace lazy.nvim with vim.pack, nvim-lspconfig with vim.lsp.config/enable,
    add native autocomplete and diagnostic configuration.
    
    * neovim: fix LSP binary names and remove loop abstraction
    
    rust_analyzer binary is rust-analyzer (hyphen), ts_ls binary is
    typescript-language-server --stdio. Inline configs since cmd doesn't
    always match the config key. Drop redundant root_markers (default).

--- neovim/config/init.lua
+++ neovim/config/init.lua
@@ -1,63 +1,66 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 vim.g.mapleader = " "
 
-require("lazy").setup({
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "v0.2.2",    dependencies = {
-      { "nvim-lua/plenary.nvim", tag = "v0.1.4" },    },
-    keys = {
-      { "<leader>f", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-      { "<leader>g", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
-      { "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+vim.pack.add({
+  "https://github.com/nvim-telescope/telescope.nvim",
+  "https://github.com/nvim-lua/plenary.nvim",
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+  "https://github.com/christoomey/vim-tmux-navigator",
+})
+
+vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
+vim.keymap.set("n", "<leader>g", "<cmd>Telescope live_grep<cr>", { desc = "Live grep" })
+vim.keymap.set("n", "<leader>b", "<cmd>Telescope buffers<cr>", { desc = "Buffers" })
+
+require("nvim-treesitter.configs").setup({
+  ensure_installed = {
+    "bash", "dockerfile", "go", "gomod",
+    "hcl", "javascript", "json", "lua",
+    "python", "rust", "terraform", "toml",
+    "typescript", "tsx", "yaml",
+  },
+  highlight = { enable = true },
+})
+
+vim.lsp.config.gopls = {
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod" },
+}
+
+vim.lsp.config.ts_ls = {
+  cmd = { "typescript-language-server", "--stdio" },
+  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+}
+
+vim.lsp.config.ty = {
+  cmd = { "ty" },
+  filetypes = { "python" },
+}
+
+vim.lsp.config.rust_analyzer = {
+  cmd = { "rust-analyzer" },
+  filetypes = { "rust" },
+}
+
+vim.lsp.config.terraformls = {
+  cmd = { "terraformls" },
+  filetypes = { "terraform", "terraform-vars" },
+}
+
+vim.lsp.enable({ "gopls", "ts_ls", "ty", "rust_analyzer", "terraformls" })
+
+vim.o.autocomplete = true
+vim.o.pumborder = "rounded"
+vim.o.completeopt = "menu,menuone,noselect,nearest"
+
+vim.diagnostic.config({
+  severity_sort = true,
+  virtual_text = { spacing = 2 },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "E",
+      [vim.diagnostic.severity.WARN] = "W",
+      [vim.diagnostic.severity.INFO] = "I",
+      [vim.diagnostic.severity.HINT] = "H",
     },
   },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    tag = "v0.10.0",    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "bash", "dockerfile", "go", "gomod",
-          "hcl", "javascript", "json", "lua",
-          "python", "rust", "terraform", "toml",
-          "typescript", "tsx", "yaml",
-        },
-        highlight = { enable = true },
-      })
-    end,
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    tag = "v2.7.0",    config = function()
-      local lspconfig = require("lspconfig")
-      local servers = { "gopls", "ts_ls", "ty", "rust_analyzer", "terraformls" }
-      for _, server in ipairs(servers) do
-        lspconfig[server].setup({})
-      end
-
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local opts = { buffer = args.buf }
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-          vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
-          vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, opts)
-        end,
-      })
-    end,
-  },
-
-  { "christoomey/vim-tmux-navigator", tag = "v1.0" },})
+})
