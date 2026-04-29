beb8f49d49615118dd08fe4f1ad300dca43526ed
Ben Drucker <bvdrucker@gmail.com>
2026-04-29 16:14:05 -0700

    neovim: add lualine, gitsigns, and visual polish

--- neovim/config/lua/config/colorscheme.lua
+++ neovim/config/lua/config/colorscheme.lua
@@ -1,10 +1,26 @@
 require("catppuccin").setup({
   flavour = "auto",
   background = { light = "latte", dark = "mocha" },
+  styles = {
+    comments = { "italic" },
+    conditionals = { "italic" },
+    keywords = { "italic" },
+  },
   integrations = {
     treesitter = true,
     telescope = { enabled = true },
-    native_lsp = { enabled = true },
+    native_lsp = {
+      enabled = true,
+      underlines = {
+        errors = { "undercurl" },
+        hints = { "undercurl" },
+        warnings = { "undercurl" },
+        information = { "undercurl" },
+      },
+    },
+    gitsigns = true,
+    cmp = true,
+    mini = { enabled = true },
   },
 })
 
