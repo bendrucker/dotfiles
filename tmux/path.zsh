f06cf9cc9464cff175176343a85e4889e86a56e4
Ben Drucker <bvdrucker@gmail.com>
2026-06-27 18:34:15 -0700

    tmux: export TMUX_TMPDIR in path.zsh for non-interactive shells
    Setting TMUX_TMPDIR only in tmux.zsh (sourced by zshrc) left non-interactive shells on the stock /tmp socket. SSH clients like RootShell landed on a separate tmux server with none of the real sessions. Moving the export to path.zsh (sourced by zshenv) makes every shell agree on ~/.tmux.

--- tmux/path.zsh
+++ tmux/path.zsh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
 
+# In path.zsh (sourced by zshenv) so non-interactive shells get it too.
+export TMUX_TMPDIR="$HOME/.tmux"
+[[ -d "$TMUX_TMPDIR" ]] || mkdir -p "$TMUX_TMPDIR"
+
 for d in $ZSH/tmux/*/bin(N); do
   export PATH="$d:$PATH"
 done
