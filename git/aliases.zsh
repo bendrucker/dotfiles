be515c09fb1ace563fd0ddd70e84bf89eb5bc5f9
Ben Drucker <bvdrucker@gmail.com>
2026-07-10 18:52:35 -0700

    git,github: add data-backed aliases from history analysis
    Fill out the git alias family (ga gd gf gl grs gsw) for high-frequency subcommands typed in full, and add gh pr aliases (ghm ghd ghv) in a new github/aliases.zsh. no_complete_aliases preserves each command's completion.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,11 +2,17 @@
 
 alias g='git'
 
+alias ga='git add'
 alias gb='git branch'
 alias gc='git commit'
 alias gco='git checkout'
+alias gd='git diff'
+alias gf='git fetch'
+alias gl='git log'
 alias gp='git push'
 alias gr='git rebase'
+alias grs='git reset'
 alias gs='git status'
+alias gsw='git switch'
 
 alias lg='lazygit'
---
+++ github/aliases.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+alias ghm='gh pr merge'
+alias ghd='gh pr diff'
+alias ghv='gh pr view'
